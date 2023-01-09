b2f9cc1ece61530d3dbbaea5f14b0b1c3e269829
JSP <mellowcroc@gmail.com>
2023-01-10 00:25:22 +0900

    Fix setting incorrect public key

--- src/useConnect.tsx
+++ src/useConnect.tsx
@@ -1,14 +1,14 @@
-import { useCallback, useState } from 'react';
-import { LeoAIP1193Wrapper } from './aip1193';
+import { useCallback, useState } from "react";
+import { LeoAIP1193Wrapper } from "./aip1193";
 
 const useConnect = (wallet: LeoAIP1193Wrapper) => {
-  const [publicKey, setPublicKey] = useState();
+  const [publicKey, setPublicKey] = useState("");
 
   const connect = useCallback(async () => {
     try {
       await wallet.connect();
-      console.log('wallet.publicKey: ', wallet.publicKey);
-      setPublicKey(publicKey);
+      console.log("wallet.publicKey: ", wallet.publicKey);
+      setPublicKey(wallet.publicKey);
     } catch (err) {
       console.log(err);
     }
